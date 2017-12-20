7d6b702d5c137c18ae1f210060c15c69a54b630d
Kawa Yg <yangjiahua19920809@gmail.com>
2017-12-20 17:13:14 +0800

    stack

---
+++ stack/stack.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/20 下午5:02
+# @Author  : KaWa
+# @File    : stack.py
+# @Project : dl-demo
+# @Copyright(c) 2017 By KaWa All rights reserved.
+
+"""""""""""
+A module to implement the stack
+"""""""""""
+
+# library
+# standard library
+
+# third-party library
+
+
+class Stack:
+    def __init__(self):
+        self._list = []
+
+    def push(self, item):
+        """push item to the stack"""
+        self._list.append(item)
+
+    def pop(self):
+        """stack pop a item"""
+        self._list.pop()
+
+    def peek(self):
+        """return the top item of the stack"""
+        if self._list:
+            return self._list[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        """stack is empty or not"""
+        return not self._list          # or return self._list == []
+
+    def size(self):
+        """return the item number of the stack"""
+        return len(self._list)
---
+++ stack/tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/20 下午5:09
+# @Author  : KaWa
+# @File    : tests.py
+# @Project : dl-demo
+# @Copyright(c) 2017 By KaWa All rights reserved.
+
+from stack import Stack
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    print(s.peek())
+    s.push(3)
+    s.push(4)
+    print(s.size())
+
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
