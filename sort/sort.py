036e86f78fa656d39d32432e0e2025e7fb8ca9dc
Kawa Yg <yangjiahua19920809@gmail.com>
2017-12-20 18:02:39 +0800

    optimize bubble sort

--- sort/sort.py
+++ sort/sort.py
@@ -9,6 +9,10 @@ def bubble_sort(alist):
     """bubble sort"""
     n = len(alist)
     for j in range(n-1):
+        count = 0
         for i in range(0, n-1-j):
             if alist[i] > alist[i+1]:
                 alist[i], alist[i+1] = alist[i+1], alist[i]
+                count += 1
+        if 0 == count:
+            return
